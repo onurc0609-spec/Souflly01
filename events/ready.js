674a36542e609734b0a1044b22c7d32f7a87063c
onurc0609-spec <onurc0609@gmail.com>
2026-02-18 07:58:18 +0300

    Create events/ready.js file

---
+++ events/ready.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'ready',
+  once: true,
+  execute(client) {
+    console.log(`✅ Bot giriş yaptı: ${client.user.tag}`);
+    client.user.setActivity('Souflly', { type: 'WATCHING' });
+  },
+};
